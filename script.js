aeff4cf7c56d947098c9f7aef0ffdc786879ef7b
Lennert Verwimp <madnessofgodzz@gmail.com>
2021-03-30 15:53:51 +0200

    added table.css, added delay, animation to header

--- script.js
+++ script.js
@@ -7,19 +7,17 @@ let dropmenu = document.querySelector('.oceanwp-mobile-menu-icon');
     header.classList.toggle('scrolling-active', window.scrollY>0);
 });
 let kutrommel = document.getElementsByClassName('mobile-menu');
-console.log(kutrommel[0]);
 kutrommel[0].addEventListener('click', function(){
     let header = document.querySelector('header');
     let dropmenu = document.querySelector('.oceanwp-mobile-menu-icon');
-    console.log('kgraak tot hier');
     if(kutrommel[0].classList.contains('opened')){
-        console.log('opened');
-        header.classList.remove('scrolling-active');
-        dropmenu.classList.remove('scrolling-active')
+        setTimeout(() => {
+            header.classList.remove('scrolling-active');
+            dropmenu.classList.remove('scrolling-active')
+        }, 500);
     }else
     {
         header.classList.add('scrolling-active');
         dropmenu.classList.add('scrolling-active');
     };
-
 });
