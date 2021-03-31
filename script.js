c32c5de9ada6b434be556ca2f15bdb44bf3860c5
Lennert Verwimp <madnessofgodzz@gmail.com>
2021-03-31 10:03:02 +0200

    header finalised, table responsive card design

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function(){  
     let header = document.querySelector('header');
-let menu = document.querySelector('ul');
-let dropmenu = document.querySelector('.oceanwp-mobile-menu-icon');
+    let menu = document.querySelector('ul');
+    let dropmenu = document.querySelector('.oceanwp-mobile-menu-icon');
     menu.classList.toggle('scrolling-nav', window.scrollY>0);
     dropmenu.classList.toggle('scrolling-active', window.scrollY>0);
     header.classList.toggle('scrolling-active', window.scrollY>0);
@@ -12,12 +12,12 @@ kutrommel[0].addEventListener('click', function(){
     let dropmenu = document.querySelector('.oceanwp-mobile-menu-icon');
     if(kutrommel[0].classList.contains('opened')){
         setTimeout(() => {
-            header.classList.remove('scrolling-active');
-            dropmenu.classList.remove('scrolling-active')
+            header.classList.remove('scrollmenu');
+            dropmenu.classList.remove('scrollmenu')
         }, 500);
     }else
     {
-        header.classList.add('scrolling-active');
-        dropmenu.classList.add('scrolling-active');
+        header.classList.add('scrollmenu');
+        dropmenu.classList.add('scrollmenu');
     };
 });
